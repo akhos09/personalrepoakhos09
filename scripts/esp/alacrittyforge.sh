c8780d5e15194758fb64ae3247e3da079449b685
Ak740s <150979654+akhos09@users.noreply.github.com>
2024-12-03 10:28:41 +0100

    Update alacrittyforge.sh

--- scripts/esp/alacrittyforge.sh
+++ scripts/esp/alacrittyforge.sh
@@ -14,11 +14,6 @@
 #:        - "CMake", "Pkg-config", "libfreetype6-dev", "libfontconfig1-dev", "Cargo", "libxcb-xfixes0-dev"
 #:        - "libxkbcommon-dev", "Python3", "libglib2.0-dev", "libgdk-pixbuf2.0-dev", "libxi-dev", "libxrender-dev", "libxrandr-dev", "libxinerama-dev"
 #:Créditos: @chrisduerr y @kchibisov creadores de Alacritty.
-
-
-# Comprobación de conectividad
-if ping -c 1 8.8.8.8 &> /dev/null; then
-    # Si hay conexión, mostrar "AlacrittyForge" en ASCII
     echo -e "\n-----------------------------------------------------------------------------------------------------------------"
     echo -e "-----------------------------------------------------------------------------------------------------------------\n"
     echo "  █████╗ ██╗      █████╗  ██████╗██████╗ ██╗████████╗████████╗██╗   ██╗███████╗ ██████╗ ██████╗  ██████╗ ███████╗"
@@ -29,34 +24,27 @@ if ping -c 1 8.8.8.8 &> /dev/null; then
     echo " ╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝╚═╝   ╚═╝      ╚═╝      ╚═╝   ╚═╝      ╚═════╝ ╚═╝  ╚═╝ ╚═════╝ ╚══════╝"
     echo -e "\n-----------------------------------------------------------------------------------------------------------------"
     echo -e "-----------------------------------------------------------------------------------------------------------------\n"
-else
-    echo "No hay conexión a Internet. Revise la configuración del adaptador y vuelva a ejecutar el script."
-    exit 1  # Salir del script si no hay conexión
-fi
-
-
-# Función para instalar Alacritty sin actualizar
+    
+#Función para instalar Alacritty sin actualizar
 install_alacritty() {
     echo -e "\nInstalando Alacritty sin actualizar los paquetes..."
-
-    # Instalación de dependencias
     sudo apt install -y cmake pkg-config libfreetype6-dev libfontconfig1-dev cargo \
     libxcb-xfixes0-dev libxkbcommon-dev python3 libglib2.0-dev \
     libgdk-pixbuf2.0-dev libxi-dev libxrender-dev libxrandr-dev libxinerama-dev
-
-    # Instalación de Rust
+    
+    #Instalación de Rust
     curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
     source $HOME/.cargo/env
     sudo apt install -y build-essential
 
-    # Clonamos el repositorio y nos quedamos en el directorio del repositorio
+    #Clonamos el repositorio y nos quedamos en el directorio del repositorio
     git clone https://github.com/alacritty/alacritty.git 2>/dev/null
     cd alacritty || { echo "No se pudo cambiar al directorio de Alacritty."; exit 1; }
-
-    # Instalación con cargo
+    
+    #Instalación con cargo
     cargo build --release
 
-    # Creación de icono en la GUI
+    #Creación de icono en la GUI
     sudo cp target/release/alacritty /usr/local/bin 
     sudo cp extra/logo/alacritty-term.svg /usr/share/pixmaps/Alacritty.svg
     sudo desktop-file-install extra/linux/Alacritty.desktop
@@ -64,7 +52,7 @@ install_alacritty() {
 }
 
 
-# Función para actualizar y luego instalar Alacritty
+#Función para actualizar y luego instalar Alacritty
 update_and_install_alacritty() {
     echo "Actualizando los paquetes e instalando Alacritty..."
     sudo apt update && sudo apt upgrade -y
@@ -72,39 +60,32 @@ update_and_install_alacritty() {
 }
 
 
-# Función para cambiar el tema de Alacritty
+#Función para cambiar el tema de Alacritty
 change_alacritty_theme() {
     echo -e "\nSelector de temas Alacritty:"
-    # Usamos el directorio de configuración predeterminado de Linux para almacenar los temas
+    #Usamos el directorio de configuración predeterminado de Linux para almacenar los temas
     mkdir -p ~/.config/alacritty/themes 2>/dev/null
     git clone https://github.com/alacritty/alacritty-theme ~/.config/alacritty/themes 2>/dev/null
 
     echo -e "\nTemas disponibles y apariencia en el repositorio adjunto: https://github.com/alacritty/alacritty-theme"
 
-    # Solicitar al usuario que ingrese el nombre del tema
+    #Solicitar al usuario que ingrese el nombre del tema
     read -p "Ingrese el nombre del tema de Alacritty (ej. aura, blood_moon, gruvbox_dark): " theme
 
     # Verificar que el directorio y archivo del tema existen
     theme_file="$HOME/.config/alacritty/themes/themes/${theme}.toml"
     if [[ -f "$theme_file" ]]; then
-        # Archivo de configuración de Alacritty
-        config_file="$HOME/.config/alacritty/alacritty.toml"
-
-        # Crear la línea de importación en el formato correcto
+        #Archivo de configuración de Alacritty
+        config_file="$HOME/.config/alacritty/alacritty.toml
+        "
         new_import_line="import = [\"$theme_file\"]"
-
-        # Verificar si la sección de importaciones ya existe
         if grep -q "^import =" "$config_file"; then
-            # Reemplazar la línea existente de importación
             sed -i "s|^import = .*|$new_import_line|" "$config_file"
             echo "Tema '$theme' reemplazado exitosamente en la configuración."
         else
-            # Si no existe, agregar la nueva línea de importación
             echo -e "$new_import_line\n" >> "$config_file"
             echo "Tema '$theme' agregado exitosamente a la configuración."
         fi
-
-        # Migra el archivo para hacer instantáneo el cambio de temas
         echo "Iniciando Alacritty con el tema '$theme'..."
         alacritty migrate
     else
@@ -113,8 +94,6 @@ change_alacritty_theme() {
     fi
 }
 
-
-# Menú de selección
 while true; do
     echo "Seleccione una opción:"
     echo "1) Instalar Alacritty sin comprobar las actualizaciones."
